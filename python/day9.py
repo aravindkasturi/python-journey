91aeddd3c7ec6356762464749b5b105dc747e56f
aravindkasturi <aravindkasturi68@gmail.com>
2026-07-15 14:37:41 +0530

    day9

---
+++ python/day9.py
@@ -0,0 +1,22 @@
+#Dictionaries
+#{key:value}
+
+ex = {
+"bug":"An error in program that prevents program from running as execpted", 
+"Function":"A piece of code that u can call over again and again",
+"Loop":"The action of doing something again and again"
+}
+# print(ex["bug"])
+# ex["bug"]="Nothing"
+# print(ex)
+
+# empty_dict={}
+# ex={}
+# print(ex)
+for _ in ex:
+    print(_) #keys will be printed 
+    print(ex[_])
+
+
+
+
