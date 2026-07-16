660dc490ab4f3ae6ed761cd346617b2c408ac10a
aravindkasturi <aravindkasturi68@gmail.com>
2026-07-16 20:38:52 +0530

    day10

--- python/day10.py
+++ python/day10.py
@@ -1,5 +1,12 @@
 #fun with outputs
-def my_fun():
-    result=3*2
-    return result
-print(my_fun())
+
+# def my_fun():
+#     result=3*2
+#     return result
+# print(my_fun())
+
+def format_name(f_name,l_name):
+    # f_name=f_name.title()
+    # l_name=l_name.title()
+    print(f"Name is {f_name.title()} {l_name.title()}")
+format_name(input("Enter first name: "),input("Enter last name: "))
