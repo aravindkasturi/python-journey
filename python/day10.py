0f6c428f7694e34c686d912375f8c78fff095ac8
aravindkasturi <aravindkasturi68@gmail.com>
2026-07-16 20:50:02 +0530

    day10

--- python/day10.py
+++ python/day10.py
@@ -8,5 +8,8 @@
 def format_name(f_name,l_name):
     # f_name=f_name.title()
     # l_name=l_name.title()
-    print(f"Name is {f_name.title()} {l_name.title()}")
-format_name(input("Enter first name: "),input("Enter last name: "))
+    if f_name=="" or l_name=="":
+        return  # means if no input it will skip the rest 
+    return f"Name is {f_name.title()} {l_name.title()}"
+out=format_name(input("Enter first name: "),input("Enter last name: "))
+print(out)
