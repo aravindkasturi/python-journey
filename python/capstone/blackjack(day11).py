a073f308759a5ca19cb9c397fcf761e8b0828d50
aravindkasturi <aravindkasturi68@gmail.com>
2026-07-18 15:23:34 +0530

    capstone

--- python/capstone/blackjack(day11).py
+++ python/capstone/blackjack(day11).py
@@ -8,34 +8,35 @@ def blackjack():
     sum_user=0
     sum_computer=0
     computer=[]
-    count=0
-    while count<2:
-        card= random.choice(cards)
-        user.append(card)
-        com_card=random.choice(cards)
-        computer.append(com_card)
-        count+=1
-    sum_user=sum(user)
-    sum_computer=sum(computer)
-    print(f"Your cards: {user}, current score: {sum_user}")
-    print(f"Computer's first card: {computer[0]}")
-    another=input("Type 'y' to get another card, type 'n' to pass: ").lower()
-    if another=='y':
+    while (sum_computer and sum_user)<17:
+        count=0
+        while count<2:
             card= random.choice(cards)
             user.append(card)
-            if sum_computer<17:
-                com_card=random.choice(cards)
-                computer.append(com_card)
-            sum_user=sum(user)
-            sum_computer=sum(computer)
-            print(f"Your cards: {user}, current score: {sum_user}")
-            print(f"Computer's first card: {computer[0]}")
-    elif another=='n':
-            if sum_computer<17:
-                com_card=random.choice(cards)
-                computer.append(com_card)
-                print(f"Your cards: {user}, current score: {sum(user)}")
+            com_card=random.choice(cards)
+            computer.append(com_card)
+            count+=1
+        sum_user=sum(user)
+        sum_computer=sum(computer)
+        print(f"Your cards: {user}, current score: {sum_user}")
+        print(f"Computer's first card: {computer[0]}")
+        another=input("Type 'y' to get another card, type 'n' to pass: ").lower()
+        if another=='y':
+                card= random.choice(cards)
+                user.append(card)
+                if sum_computer<17:
+                    com_card=random.choice(cards)
+                    computer.append(com_card)
+                sum_user=sum(user)
+                sum_computer=sum(computer)
+                print(f"Your cards: {user}, current score: {sum_user}")
                 print(f"Computer's first card: {computer[0]}")
+        elif another=='n':
+                if sum_computer<17:
+                    com_card=random.choice(cards)
+                    computer.append(com_card)
+                    print(f"Your cards: {user}, current score: {sum(user)}")
+                    print(f"Computer's first card: {computer[0]}")
     final_user=abs(21-sum_user)
     final_computer=abs(21-sum_computer)
     winner=min(final_user,final_computer)
@@ -45,12 +46,8 @@ def blackjack():
         print("You won, Congrats!")
     else:
         print("You lost")
-start=input("Do you want to start the game 'y' for yes , 'n' for no: ").lower()
-again=True
-while True:
+again = input("Do you want to start the game 'y' for yes , 'n' for no: ").lower()
+
+while again == 'y':
     blackjack()
-    
-    if start=='y':
-         again=True
-    else:
-         again=False
+    again = input("Do you want to play again? 'y' or 'n': ").lower()
