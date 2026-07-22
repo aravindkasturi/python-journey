a6664c65ff3d362ad700c3fa05d255da8b293a0b
aravindkasturi <aravindkasturi68@gmail.com>
2026-07-22 21:34:45 +0530

    'day13'

--- python/day13.py
+++ python/day13.py
@@ -32,3 +32,23 @@
 # finally:
 #     print(f"your age is {age}")
 
+# words_per_page=0
+# pages=int(input("Number of pages: "))
+# words_per_page=int(input("Enter words per page: "))
+# total_words=pages*words_per_page
+# print(total_words)
+
+#4 use a debugger
+
+import random
+
+def mutate(a_list):
+    b_list=[]
+    new_item=0
+    for item in a_list:
+        new_item=item*2
+        new_item+=random.randint(1,3)
+        new_item=(new_item+item)
+        b_list.append(new_item)
+    print(b_list)
+mutate([1,2,3,4,5])
