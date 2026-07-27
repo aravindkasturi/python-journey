84a0cf34c8ae8dfb88de944444ed7d1393868a1c
aravindkasturi <aravindkasturi68@gmail.com>
2026-07-27 17:52:17 +0530

    'day17'

---
+++ python/Practice/day17.py
@@ -0,0 +1,19 @@
+# #classes --blue print
+# class User:   #class name should be in PascalCase
+#     def __init__(self,id,username):  #constructor -- initialise attributes
+#         self.id=id
+#         self.username=username
+#         self.followers=0
+#         self.following=0
+#     #methods
+#     def follow(self, user):
+#         user.followers+=1
+#         self.following+=1
+
+        
+# user_1 = User("001","aravind")
+# user_2 = User("002","virat")
+# user_1.follow(user_2)
+# print(user_1.following)
+# print(user_2.followers)
+# #attributes --- properties  or variable associated with objects
