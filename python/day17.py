264d42a759de2f3cbc00a53ec326c709b6b8cd00
aravindkasturi <aravindkasturi68@gmail.com>
2026-07-27 17:34:27 +0530

    'day17'

--- python/day17.py
+++ python/day17.py
@@ -3,8 +3,11 @@ class User:   #class name should be in PascalCase
     def __init__(self,id,username):  #constructor -- initialise attributes
         self.id=id
         self.username=username
+        self.followers=0
+    #methods
+
         
 user_1 = User("001","aravind")
 user_2 = User("002","virat")
-print(user_2.username)
+print(user_2.followers)
 #attributes --- properties  or variable associated with objects
