2a0541df2be5e5b184f293119ec850f6759c3e02
aravindkasturi <aravindkasturi68@gmail.com>
2026-08-02 14:54:50 +0530

    day21

--- python/projects/snake_game/main.py
+++ python/projects/snake_game/main.py
@@ -6,11 +6,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)  #Turns turtle animation on/off and set delay for update drawings.
+screen.listen()
 
 snake=Snake()
-
-
-
+screen.onkey(key="Up", fun=snake.move_up)
+screen.onkey(key="Down", fun=snake.move_down)
+screen.onkey(key="Left", fun=snake.move_left)
+screen.onkey(key="Right", fun=snake.move_right)
 game_is_on=True
 while game_is_on:
     screen.update()
--- python/projects/snake_game/snake.py
+++ python/projects/snake_game/snake.py
@@ -1,10 +1,17 @@
 from turtle import  Turtle
 POS=[(0,0),(-20,0),(-40,0)]
 MOVE_DIST=20
+UP=90
+DOWN=270
+LEFT=180
+RIGHT=0
 class Snake:
+
     def __init__(self):
         self.turtle_list=[] 
         self.create_snake()
+        self.head=self.turtle_list[0]
+
     def create_snake(self):
         for turtle in range(3):
             ak=Turtle("square")
@@ -18,11 +25,19 @@ class Snake:
             new_x=self.turtle_list[i-1].xcor()
             new_y=self.turtle_list[i-1].ycor()
             self.turtle_list[i].goto(x=new_x,y=new_y)
-        self.turtle_list[0].forward(MOVE_DIST)
+        self.head.forward(MOVE_DIST)
 
-
-
-
-
+    def move_up(self):
+        if self.head.heading() != DOWN: 
+            self.head.setheading(UP)
+    def move_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def move_left(self):
+        if self.head.heading() != RIGHT: 
+            self.head.setheading(LEFT)
+    def move_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
 
