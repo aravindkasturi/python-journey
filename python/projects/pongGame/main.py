a07ee7a3bbc9a3386b8416b9a79113be9f3b1dd2
aravindkasturi <aravindkasturi68@gmail.com>
2026-08-04 22:31:20 +0530

    day22 project

---
+++ python/projects/pongGame/main.py
@@ -0,0 +1,30 @@
+from turtle import Turtle,Screen
+
+screen=Screen()
+screen.setup(600,600)
+screen.bgcolor("black")
+screen.title("PONG")
+
+paddale=Turtle()
+paddale.penup()
+paddale.shape("square")
+paddale.shapesize(stretch_wid=5,stretch_len=1)
+paddale.color("white")
+paddale.goto(270,0)
+def move():
+    paddale.forward(10)
+def move_up():
+    new_y=paddale.ycor()+20
+    paddale.goto(paddale.xcor(),new_y)
+def move_down():
+    new_y=paddale.ycor()-20
+    paddale.goto(paddale.xcor(),new_y)
+screen.listen()
+screen.onkey(move_up,"Up")
+screen.onkey(move_down,"Down")
+
+
+
+
+
+screen.exitonclick()
